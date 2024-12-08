caaf0dc5a61e16b0248f54135c34bfbedeb65445
AmeeshaAntony <ameesha468@gmail.com>
2024-12-08 10:38:12 +0530

    second commit

---
+++ src/app/components/roles/roles.component.ts
@@ -0,0 +1,35 @@
+import { HttpClient } from '@angular/common/http';
+import { Component , inject, OnInit } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { IRole } from '../model/interface/role';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-roles',
+  imports: [FormsModule,CommonModule],
+  templateUrl: './roles.component.html',
+  styleUrl: './roles.component.css'
+})
+
+export class RolesComponent implements OnInit {
+  /*firstName : string = "Angular Tutorial";
+  angularVersion = "Version 18";
+  version : number = 18;
+  isActive : boolean = false;
+  cD : Date = new Date();
+  showalert(alert1 : string){
+    alert(alert1)
+  }*/
+  roleList: IRole [] = [];
+  http = inject(HttpClient);
+  ngOnInit(): void {
+    this.getRoles()
+  }
+  getRoles(){
+    this.http.get("https://freeapi.miniprojectideas.com/api/ClientStrive/GetAllRoles").subscribe((res:any)=>{
+      this.roleList = res.data
+    })
+  }
+
+  
+}
